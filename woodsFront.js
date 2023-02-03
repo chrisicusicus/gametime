80aacdf568b6bcdcf54a7396cbdbfe1e9e9093e2
chrisicusicus <115127962+chrisicusicus@users.noreply.github.com>
2023-02-03 00:55:57 -0500

    added inv

--- woodsFront.js
+++ woodsFront.js
@@ -1,4 +1,24 @@
-$(".appendbtn").click(function () {
-    var template = $('#appendTemplate").html();
-    $(".appendme").append(template);
-});
+// $(".appendbtn").click(function () {
+//     var template = $('#appendTemplate").html();
+//     $(".appendme").append(template);
+// });
+
+
+const textNarrator = document.getElementById('NarratorText')
+const textOptions = document.getElementById('optionBoxes')
+const optionBox1 = document.getElementById('option1')
+
+function block() {
+    let div = document.getElementById('char1');
+    if (div.style.display !== 'none') {
+      div.style.display = 'block';
+    }
+  
+  };
+
+  function newText(){
+    document.getElementById("NarratorText").innerHTML = "new text";
+  }
+
+
+
